1e99729fbfff3b381d2dee99c4d8ea43b5f75c17
Andy Zhou <andy@copilotup.com>
2024-10-06 01:33:06 -0400

    added a training script

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -1,1 +1,2 @@
 from .generate import *
+from .train import *
---
+++ src/pipeline/train.py
@@ -0,0 +1,52 @@
+import torch
+import math
+from torch.utils.data import Dataset, DataLoader
+from tqdm import trange, tqdm
+
+from ..modules import GPT2, GPT2Output
+
+
+def pad_num(num: int, padding_multiple: int = 2):
+    digits = 1 if num == 0 else int(math.log10(num)) + 1
+    padding_required = ((digits // padding_multiple) + 1) * padding_multiple
+    return f"{num:>{padding_required}d}"
+
+
+def train_gpt2(
+    model: GPT2,
+    dataset: Dataset,
+    batch_size=4,
+    num_epochs=1,
+    lr=3e-4,
+    device="cpu",
+    generator=None,
+):
+    model.train()
+    optim = torch.optim.AdamW(model.parameters(), lr=lr)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        generator=generator,
+        drop_last=True,
+    )
+
+    for epoch in trange(num_epochs, desc="Epoch", unit=" epochs"):
+        for minibatch, (context, labels) in enumerate(
+            tqdm(dataloader, desc="Minibatch")
+        ):
+            assert isinstance(context, torch.Tensor)
+            assert isinstance(labels, torch.Tensor)
+
+            context = context.to(device)
+            labels = labels.to(device)
+
+            loss = model(context, labels=labels).loss
+            assert isinstance(loss, torch.Tensor)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            tqdm.write(
+                f"[Epoch {pad_num(epoch)}, Minibatch {pad_num(minibatch)}]: Loss={loss.item():.4f}"
+            )
